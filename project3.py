93357d5d47a59923a4a5a69ccfd3bb2a91df7a26
Karl Ryden <karlrydenn@gmail.com>
2021-12-13 15:41:53 +0100

    task 2

--- project3.py
+++ project3.py
@@ -1,6 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from numpy import diag, exp, ones, zeros, eye, dot, linspace, meshgrid, vectorize
+from numpy.linalg.linalg import _multi_dot_matrix_chain_order
+
+def RMS(v, dx):
+    return np.sqrt(dx)*np.linalg.norm(v)
 
 def eulerstep(Tdx, uold, dt):
     return uold + dt*dot(Tdx, uold)
@@ -40,6 +44,7 @@ def LaxWen(u, amu):
     d1 = amu/2*(1 + amu)
     d2 = 1 - amu**2
     d3 = -amu/2*(1 - amu)
+    # Tamu is a Toeplitz matrix
     Tamu = d1*diag(ones(N - 1), -1) + d3*diag(ones(N - 1), 1) + d2*eye(N)
     Tamu[0][-1] = d1
     Tamu[-1][0] = d3
@@ -114,24 +119,58 @@ if __name__ == '__main__':
     def task21():
         N = 99
         M = 2499
-        tf = 0.1
-        a = M/N
+        tf = 5
+        dx = 1/N
+        dt = tf/M
+        mu = dt/dx
+
+        a1 = 1
+        a2 = -0.5
+        a3 = 1/mu
+        a4 = 0.9/mu
 
         xgrid = linspace(0, 1, N + 1)
         tgrid = linspace(0, tf, M + 1)
 
         g = vectorize(lambda x: exp(-100*((x - 0.5)**2)))
-        #g = vectorize(lambda x: 0.5 - abs(x - 0.5) if (abs(x - 0.5) <= 0.25) else 0.25) # -^-
+        L2 = lambda v: RMS(v, dx)
 
         [T, X] = meshgrid(xgrid, tgrid)
-        U = LaxWenint(a, g(xgrid), tf, N, M)
-        fig = plt.figure()
-        ax = fig.add_subplot(projection='3d')
-        ax.plot_surface(T, X, U)
-        ax.set(xlabel='x')
-        ax.set(ylabel='t')
-        # ax.zlabel('u')
+        U1 = LaxWenint(a1, g(xgrid), tf, N, M)
+        U2 = LaxWenint(a2, g(xgrid), tf, N, M)
+        U3 = LaxWenint(a3, g(xgrid), tf, N, M)
+        U4 = LaxWenint(a4, g(xgrid), tf, N, M)
+        
+        print(T)
+        print(X)
+        L2U3 = np.apply_along_axis(L2, 1, U3)
+        L2U4 = np.apply_along_axis(L2, 1, U4)
+
+        fig1 = plt.figure()
+        ax1 = fig1.add_subplot(projection='3d')
+        ax1.plot_wireframe(T, X, U1)
+        ax1.set(xlabel='x')
+        ax1.set(ylabel='t')
+        ax1.set(title=f'a = {a1}')
+
+        fig2 = plt.figure()
+        ax2 = fig2.add_subplot(projection='3d')
+        ax2.plot_wireframe(T, X, U2)
+        ax2.set(xlabel='x')
+        ax2.set(ylabel='t')
+        ax2.set(title=f'a = {a2}')
+        
+        figL2 = plt.figure()
+        plt.plot(tgrid, L2U3)
+        plt.plot(tgrid, L2U4)
+        plt.xlabel('t')
+        plt.legend(['amu = 1', 'amu = 2'])
+        plt.title('L2 norm of solutions')
+
         plt.show()
+        
+        # The norm decreases with time when amu < 1. This is an information leak caused by going below the CLF-limit. 
+        # Not sure how to motivate further.
 
     def task31():
         N = 99
@@ -157,5 +196,6 @@ if __name__ == '__main__':
         plt.show()
 
     # task11()
-    task12()
+    # task12()
     # task21()
+    task31()
